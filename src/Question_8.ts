2d23a4914ec2e751a169973d6bdc9deefdb61c04
sabamusharraf <sabahussain337@gmail.com>
2024-03-30 15:11:34 +0500

    Ready to commit some more exercise

---
+++ src/Question_8.ts
@@ -0,0 +1,27 @@
+// Question # 8
+
+/* You should create four lines that look like this:
+
+console.log(5 + 3)
+
+Your output should simply be four lines with the number 8 appearing once on each line. */
+
+// By using Function Method
+
+// function sum (){
+//     console.log(5 + 3);
+// }
+
+// sum();
+// sum();
+// sum();
+// sum();
+
+// By using For Loop
+
+let i = 0;
+let n = 4;
+
+for (let i = 1; i <= n; i ++){
+    console.log(5 + 3);
+}
