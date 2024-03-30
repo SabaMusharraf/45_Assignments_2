7fd067a0364c4bda55b8c07c79d23f0ab73efd18
sabamusharraf <sabahussain337@gmail.com>
2024-03-30 17:17:19 +0500

    Add some changes

--- src/Question_10.ts
+++ src/Question_10.ts
@@ -5,13 +5,14 @@ If you don’t have anything specific to write because your programs are too sim
 your name and the current date at the top of each program file. Then write one sentence describing what the 
 program does.  */
 
+//By using Double Forword Slash (//) can make single line comment which is not accessable in the program or code...
+/* by using Forward Slash (/) and Asterisk sign (*) like this /*  can make multiple line comments */
+
 // Date: 3/30/2024
 // My name is Saba Hussain
 
 // In this program I make a function that calculate and print print the number 4 times.
 
-//By using Double Forword Slash (//) can make single line comment which is not accessable in the program or code...
-/* by using Forward Slash (/) and Asterisk sign (*) like this /*  can make multiple line comments */
 
 function sum (){
     console.log(5 + 3);
