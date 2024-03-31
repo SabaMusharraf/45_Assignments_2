5ac579de4e66ea01b94bea71730429cd3d6c66e0
sabamusharraf <sabahussain337@gmail.com>
2024-03-31 11:46:20 +0500

    Add more exercise

---
+++ src/Question_16.ts
@@ -0,0 +1,25 @@
+// Question # 16
+
+/* More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests 
+to invite to dinner.
+• Start with your program from Exercise 15. Add a print statement to the end of your program informing people 
+that you found a bigger dinner table.
+
+• Add one new guest to the beginning of your array.
+
+• Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. 
+• Print a new set of invitation messages, one for each person in your list. */
+
+let guestList_2 = ["Maryam", "Hina", "Iffat"];
+
+let newMessage = "I want to invite morre guest because I found bigger dinning table";
+
+// Add guest name to the beggining of the array
+
+guestList_2.unshift("Fatima");
+console.log(guestList_2);
+
+// Add guest name to  middle of the array
+
+guestList_2.splice(2,0,"Aamna");
+console.log(guestList_2);
