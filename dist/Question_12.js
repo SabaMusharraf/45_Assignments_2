0cd59caa689924f70b491c16cb870fbe0035c99e
sabamusharraf <sabahussain337@gmail.com>
2024-03-31 05:45:45 +0500

    Add some Exercise

--- dist/Question_12.js
+++ dist/Question_12.js
@@ -4,7 +4,13 @@
 print a message to them. The text of each message should be the same, but each message should be personalized
 with the person’s name. */
 let name_1 = ["Maryam", "Hina", "Maira", "Iffat", "Rabia"];
-let greetings = "Asssalam O Alaikum";
+let greetings = "Asssalam O Alaikum, how are you?";
+//For Loop
 for (let i = 0; i < name_1.length; i++) {
-    console.log(`${greetings} ${name_1[i]}`);
+    console.log(`${greetings}, ${name_1[i]}`);
 }
+// For Each Loop
+let friendsName = ["Maryam", "Hina", "Maira", "Iffat", "Rabia"];
+friendsName.forEach((val) => {
+    console.log(greetings, val);
+});
