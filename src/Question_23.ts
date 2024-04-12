00ed2e57e5896e0b39d173d65ae8a477245e4b4a
sabamusharraf <sabahussain337@gmail.com>
2024-04-12 23:14:01 +0500

    Add commit

--- src/Question_23.ts
+++ src/Question_23.ts
@@ -13,8 +13,44 @@ console.log(car == 'subaru')
 
 • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. */
 
-let governorHouse = "onsite students";
+// let governorHouse = "onsite students";
 
-let result = governorHouse == "onsite students" ? "true": "false";
+// let result = governorHouse == "onsite students" ? "true": "false";
 
-console.log(result);
+// console.log(result);
+
+let x = 10;
+let y = 5;
+
+// console.log("Test 1 to 5 be True");
+let test_1 = x !== y ? "true": "false";
+console.log(test_1);
+
+let test_2 = x > y ? "true": "false";
+console.log(test_2);
+
+let test_3 = y < x ? "true": "false";
+console.log(test_3);
+
+let test_4 = x + y == 15 ? "true": "false";
+console.log(test_4);
+
+let test_5 = x - y == 5 ? "true": "false";
+console.log(test_5);
+
+// console.log("Test 6 to 10 be False");
+
+let test_6 = x == y ? "true": "false";
+console.log(test_6);
+
+let test_7 = x === y ? "true": "false";
+console.log(test_7);
+
+let test_8 = x < y ? "true": "false";
+console.log(test_8);
+
+let test_9 = x !< y ? "tue": "false";
+console.log(test_9);
+
+let test_10 = y > x ? "true": "false";
+console.log(test_10);
