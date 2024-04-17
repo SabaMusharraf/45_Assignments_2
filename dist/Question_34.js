7df7eebe66f0802cf2c4d0378e9455a104f0900a
sabamusharraf <sabahussain337@gmail.com>
2024-04-17 16:36:37 +0500

    Add some more solved Questions

--- dist/Question_34.js
+++ dist/Question_34.js
@@ -8,4 +8,9 @@ like I like pepperoni pizza.
 
 • Add a line at the end of your program, outside the for loop, that states how much you like pizza.
 The output should consist of three or more lines about the kinds of pizza you like and then an additional
-sentence, such as I really love pizza! */ 
+sentence, such as I really love pizza! */
+let pizzas = ["Chicken Tikka Pizza", "Pepperoni Pizza", "Cheese Pizza"];
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza}.`);
+}
+console.log("I really love pizza.");
